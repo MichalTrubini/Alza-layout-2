0317d5c727943d1134d80b57b12ade6a194f7dc3
Michal Trubini <mchl.trubini@gmail.com>
2022-11-28 18:12:55 +0100

    WIP

--- script.js
+++ script.js
@@ -75,6 +75,39 @@ sliders.forEach((item, i) => {
 /*	 3.TABLE
 /*-----------------------------------------------------------------------------------*/
 
+const tableHeader = document.querySelectorAll('.table__headerItemContainer ');
+const tableItems = document.querySelectorAll('.table__item');
+
+tableItems.forEach((item, i) => {
+
+    tableHeader[i].addEventListener("click", () => {
+
+        tableItems.forEach(tableItem => {
+            if (tableItem.classList.contains('table__item--visible')) tableItem.classList.remove('table__item--visible');
+        })
+
+        item.classList.add('table__item--visible');
+        item.parentElement.style.height = item.offsetHeight + "px"
+    });
+  
+})
+
+tableHeader.forEach(item => {
+    item.addEventListener('click', () => {
+
+        tableHeader.forEach(item => {
+            if (item.children[1].classList.contains('table__headerItemRectangle--visible')) {item.children[1].classList.remove('table__headerItemRectangle--visible'); item.children[0].classList.remove('table__headerItem--selected')}
+        })
+
+        item.children[1].classList.add('table__headerItemRectangle--visible');
+        item.children[0].classList.add('table__headerItem--selected')
+    })
+})
+
+const tableItemVisible = document.querySelector('.table__item--visible');
+
+tableItemVisible.parentElement.style.height = tableItemVisible.offsetHeight + "px"
+
 /*-----------------------------------------------------------------------------------*/
 /*	 4.ANIMATIONS WHEN SCROLLED TO VIEW
 /*-----------------------------------------------------------------------------------*/
@@ -86,17 +119,67 @@ let observer = new IntersectionObserver(entries => {
     const headerTopBar = entries.filter(entry => entry.target.classList.contains('header-top-border'))
     const imageAnimateLeft = entries.filter(entry => entry.target.classList.contains('image-wrapper-right'))
     const imageAnimateRight = entries.filter(entry => entry.target.classList.contains('image-wrapper-left'))
+    const benefitComponent = entries.filter(entry => entry.target.classList.contains('benefit-component'))
+    const block2Image = entries.filter(entry => entry.target.classList.contains('block-2-text-wrapper-right'))
+    const dividerLine = entries.filter(entry => entry.target.classList.contains('gray-divider-in-image'))
+    const circleOne = entries.filter(entry => entry.target.classList.contains('block-2-circle-imageOne'))
+    const circleTwo = entries.filter(entry => entry.target.classList.contains('block-2-circle-imageTwo'))
+    const circleThree = entries.filter(entry => entry.target.classList.contains('block-2-circle-imageThree'))
+    const benefitBlockImage = entries.filter(entry => entry.target.classList.contains('benefits-block-image'))
+    const quoteBlock = entries.filter(entry => entry.target.classList.contains('quoting'))
+    const quoteSymbol = entries.filter(entry => entry.target.classList.contains('quoting-symbol'))
+    const sliderItems = entries.filter(entry => entry.target.classList.contains('slider__item'))
+
+    sliderItems.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('slider__item-inView')
+    })
 
     headerTopBar.forEach(item => {
-        if (item.isIntersecting === true) item.target.classList.add('header-top-border-inView')
+        if (item.isIntersecting) item.target.classList.add('header-top-border-inView')
     })
 
     imageAnimateLeft.forEach(item => {
-        if (item.isIntersecting === true) item.target.classList.add('image-wrapper-right-inView')
+        if (item.isIntersecting) item.target.classList.add('image-wrapper-right-inView')
     })
 
     imageAnimateRight.forEach(item => {
-        if (item.isIntersecting === true) item.target.classList.add('image-wrapper-left-inView')
+        if (item.isIntersecting) item.target.classList.add('image-wrapper-left-inView')
+    })
+
+    benefitComponent.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('benefit-component-inView')
+    })
+
+    block2Image.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('image-wrapper-right-inView')
+    })
+
+    dividerLine.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('gray-divider-in-image-inView')
+    })
+
+    circleOne.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('block-2-circle-imageOne-inView')
+    })
+
+    circleTwo.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('block-2-circle-imageTwo-inView')
+    })
+
+    circleThree.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('block-2-circle-imageThree-inView')
+    })
+
+    benefitBlockImage.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('image-wrapper-left-inView')
+    })
+
+    quoteBlock.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('quoteBlock-inView')
+    })
+
+    quoteSymbol.forEach(item => {
+        if (item.isIntersecting) item.target.classList.add('quoting-symbol-inView')
     })
 
 });
