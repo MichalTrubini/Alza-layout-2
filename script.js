bceca40c721984534ed7cc0c37e5977745eff789
Michal Trubini <mchl.trubini@gmail.com>
2022-11-29 19:36:16 +0100

    WIP

--- script.js
+++ script.js
@@ -37,9 +37,9 @@ let sliderPadding = Number(style.padding.replace("px", ""));
 
 sliderItemsTypeA.forEach((item) => {
   if (screenWidth >= 1250) item.style.width = (sliderWidth - sliderPadding * 2) / 4 + "px"; //4 items visible
-  else if (screenWidth > 767) item.style.width = (sliderWidth - sliderPadding * 2) / 3 + "px"; // 3 items 
+  else if (screenWidth > 767) item.style.width = (sliderWidth - sliderPadding * 2) / 3 + "px"; // 3 items
   else if (screenWidth > 525) item.style.width = (sliderWidth - sliderPadding * 2) / 2 + "px"; // 2 items
-  else item.style.width = (sliderWidth - sliderPadding)+ "px"; // 1 item
+  else item.style.width = sliderWidth - sliderPadding + "px"; // 1 item
 });
 
 /*-----------------------------------------------------------------------------------*/
@@ -76,38 +76,37 @@ sliders.forEach((item, i) => {
 /*	 3.TABLE
 /*-----------------------------------------------------------------------------------*/
 
-const tableHeader = document.querySelectorAll('.table__headerItemContainer ');
-const tableItems = document.querySelectorAll('.table__item');
+const tableHeader = document.querySelectorAll(".table__headerItemContainer ");
+const tableItems = document.querySelectorAll(".table__item");
 
 tableItems.forEach((item, i) => {
-
-    tableHeader[i].addEventListener("click", () => {
-
-        tableItems.forEach(tableItem => {
-            if (tableItem.classList.contains('item--visible')) tableItem.classList.remove('item--visible');
-        })
-
-        item.classList.add('table__item--visible');
-        item.parentElement.style.height = item.offsetHeight + "px"
+  tableHeader[i].addEventListener("click", () => {
+    tableItems.forEach((tableItem) => {
+      if (tableItem.classList.contains("item--visible")) tableItem.classList.remove("item--visible");
     });
-  
-})
 
-tableHeader.forEach(item => {
-    item.addEventListener('click', () => {
+    item.classList.add("table__item--visible");
+    item.parentElement.style.height = item.offsetHeight + "px";
+  });
+});
 
-        tableHeader.forEach(item => {
-            if (item.children[1].classList.contains('table__headerItemRectangle--visible')) {item.children[1].classList.remove('table__headerItemRectangle--visible'); item.children[0].classList.remove('table__headerItem--selected')}
-        })
+tableHeader.forEach((item) => {
+  item.addEventListener("click", () => {
+    tableHeader.forEach((item) => {
+      if (item.children[1].classList.contains("table__headerItemRectangle--visible")) {
+        item.children[1].classList.remove("table__headerItemRectangle--visible");
+        item.children[0].classList.remove("table__headerItem--selected");
+      }
+    });
 
-        item.children[1].classList.add('table__headerItemRectangle--visible');
-        item.children[0].classList.add('table__headerItem--selected')
-    })
-})
+    item.children[1].classList.add("table__headerItemRectangle--visible");
+    item.children[0].classList.add("table__headerItem--selected");
+  });
+});
 
-const tableItemVisible = document.querySelector('#celek .block-10 .item--visible');
+const tableItemVisible = document.querySelector("#celek .block-10 .item--visible");
 
-tableItemVisible.parentElement.style.height = tableItemVisible.offsetHeight + "px"
+tableItemVisible.parentElement.style.height = tableItemVisible.offsetHeight + "px";
 
 /*-----------------------------------------------------------------------------------*/
 /*	 4.SLIDER TYPEB
@@ -116,21 +115,33 @@ tableItemVisible.parentElement.style.height = tableItemVisible.offsetHeight + "p
 //-->variables declaration
 
 const sliderB = document.querySelectorAll("#celek .sliderB");
-const nxtBtnB = document.querySelectorAll("#celek .sliderB__arrowContainerRight");
-const preBtnB = document.querySelectorAll("#celek .sliderB__arrowContainerLeft");
-const sliderBItems = document.querySelectorAll('.sliderB__item');
+const nxtBtnB = document.querySelector("#celek .sliderB__arrowContainerRight");
+const preBtnB = document.querySelector("#celek .sliderB__arrowContainerLeft");
+const sliderBItems = document.querySelectorAll(".sliderB__item");
+const sliderCircles = document.querySelectorAll('.sliderB__circles');
+
+let clickCounter = 0;
 
 //-->functions
 
-sliderBItems.forEach((item) => {
-
-  nxtBtnB.addEventListener("click", () => {
-    item.scrollLeft += containerWidth;
+nxtBtnB.addEventListener("click", () => {
+  sliderBItems.forEach((item, i) => {
+    if ((i === clickCounter) & (i < sliderBItems.length - 1)) {
+      item.classList.remove("item--visible");
+      item.nextElementSibling.classList.add("item--visible");
+    }
   });
+  if (clickCounter < sliderBItems.length - 1) clickCounter += 1;
+});
 
-  preBtnB.addEventListener("click", () => {
-    item.scrollLeft -= containerWidth;
+preBtnB.addEventListener("click", () => {
+  sliderBItems.forEach((item, i) => {
+    if ((i === clickCounter) & clickCounter > 0) {
+      item.classList.remove("item--visible");
+      item.previousElementSibling.classList.add("item--visible");
+    }
   });
+  if (clickCounter > 0) clickCounter -= 1;
 });
 
 /*-----------------------------------------------------------------------------------*/
@@ -143,83 +154,95 @@ const elementsToAnimate = document.querySelectorAll(`[data-animate="animation"]`
 
 //declaration of intersection observer API
 
-let observer = new IntersectionObserver(entries => {
+let observer = new IntersectionObserver((entries) => {
+  //separates elements that require the same animation into categories based on their class
 
-//separates elements that require the same animation into categories based on their class
+  const headerTopBar = entries.filter((entry) => entry.target.classList.contains("header-top-border"));
+  const imageAnimateLeft = entries.filter((entry) => entry.target.classList.contains("image-wrapper-right"));
+  const imageAnimateRight = entries.filter((entry) => entry.target.classList.contains("image-wrapper-left"));
+  const benefitComponent = entries.filter((entry) => entry.target.classList.contains("benefit-component"));
+  const block2Image = entries.filter((entry) => entry.target.classList.contains("block-2-text-wrapper-right"));
+  const dividerLine = entries.filter((entry) => entry.target.classList.contains("gray-divider-in-image"));
+  const circles = entries.filter((entry) => entry.target.classList.contains("block-2-circle-image"));
+  const benefitBlockImage = entries.filter((entry) => entry.target.classList.contains("benefits-block-image"));
+  const quoteBlock = entries.filter((entry) => entry.target.classList.contains("quoting"));
+  const quoteSymbol = entries.filter((entry) => entry.target.classList.contains("quoting-symbol"));
+  const sliderItemsOne = entries.filter((entry) => entry.target.classList.contains("slider__itemOne"));
+  const sliderItemsTwo = entries.filter((entry) => entry.target.classList.contains("slider__itemTwo"));
+  const blockOneImage = entries.filter((entry) => entry.target.classList.contains("benefity-image"));
 
-    const headerTopBar = entries.filter(entry => entry.target.classList.contains('header-top-border'))
-    const imageAnimateLeft = entries.filter(entry => entry.target.classList.contains('image-wrapper-right'))
-    const imageAnimateRight = entries.filter(entry => entry.target.classList.contains('image-wrapper-left'))
-    const benefitComponent = entries.filter(entry => entry.target.classList.contains('benefit-component'))
-    const block2Image = entries.filter(entry => entry.target.classList.contains('block-2-text-wrapper-right'))
-    const dividerLine = entries.filter(entry => entry.target.classList.contains('gray-divider-in-image'))
-    const circles = entries.filter(entry => entry.target.classList.contains('block-2-circle-image'))
-    const benefitBlockImage = entries.filter(entry => entry.target.classList.contains('benefits-block-image'))
-    const quoteBlock = entries.filter(entry => entry.target.classList.contains('quoting'))
-    const quoteSymbol = entries.filter(entry => entry.target.classList.contains('quoting-symbol'))
-    const sliderItemsOne = entries.filter(entry => entry.target.classList.contains('slider__itemOne'))
-    const sliderItemsTwo = entries.filter(entry => entry.target.classList.contains('slider__itemTwo'))
-    const blockOneImage = entries.filter(entry => entry.target.classList.contains('benefity-image'))
+  //if element is intersecting (that means, in viewport), class is added to the element
+  //class then defines animation using keyframes
 
-//if element is intersecting (that means, in viewport), class is added to the element
-//class then defines animation using keyframes
+  sliderItemsOne.forEach((item, i) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("slider__item-inView");
+      item.target.style.animationDelay = 0.5 * i + "s";
+    }
+  });
 
-    sliderItemsOne.forEach((item,i) => {
-        if (item.isIntersecting) {item.target.classList.add('slider__item-inView'); item.target.style.animationDelay = 0.5 * i + "s"}
-    })
+  blockOneImage.forEach((item) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("benefity-image-inView");
+    }
+  });
 
-    blockOneImage.forEach((item) => {
-        if (item.isIntersecting) {item.target.classList.add('benefity-image-inView')}
-    })
+  sliderItemsTwo.forEach((item, i) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("slider__item-inView");
+      item.target.style.animationDelay = 0.5 * i + "s";
+    }
+  });
 
-    sliderItemsTwo.forEach((item,i) => {
-        if (item.isIntersecting) {item.target.classList.add('slider__item-inView'); item.target.style.animationDelay = 0.5 * i + "s"}
-    })
+  headerTopBar.forEach((item) => {
+    if (item.isIntersecting) item.target.classList.add("header-top-border-inView");
+  });
 
-    headerTopBar.forEach(item => {
-        if (item.isIntersecting) item.target.classList.add('header-top-border-inView')
-    })
+  imageAnimateLeft.forEach((item) => {
+    if (item.isIntersecting) item.target.classList.add("image-wrapper-right-inView");
+  });
 
-    imageAnimateLeft.forEach(item => {
-        if (item.isIntersecting) item.target.classList.add('image-wrapper-right-inView')
-    })
+  imageAnimateRight.forEach((item) => {
+    if (item.isIntersecting) item.target.classList.add("image-wrapper-left-inView");
+  });
 
-    imageAnimateRight.forEach(item => {
-        if (item.isIntersecting) item.target.classList.add('image-wrapper-left-inView')
-    })
+  benefitComponent.forEach((item, i) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("benefit-component-inView");
+      item.target.style.animationDelay = 0.5 * i + "s";
+    }
+  });
 
-    benefitComponent.forEach((item,i) => {
-        if (item.isIntersecting) {item.target.classList.add('benefit-component-inView'); item.target.style.animationDelay = 0.5 * i + "s"}
-    })
+  block2Image.forEach((item) => {
+    if (item.isIntersecting) item.target.classList.add("image-wrapper-right-inView");
+  });
 
-    block2Image.forEach(item => {
-        if (item.isIntersecting) item.target.classList.add('image-wrapper-right-inView')
-    })
+  dividerLine.forEach((item) => {
+    if (item.isIntersecting) item.target.classList.add("gray-divider-in-image-inView");
+  });
 
-    dividerLine.forEach(item => {
-        if (item.isIntersecting) item.target.classList.add('gray-divider-in-image-inView')
-    })
+  circles.forEach((item, i) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("block-2-circle-inView");
+      item.target.style.animationDelay = 0.5 * i + "s";
+    }
+  });
 
-    circles.forEach((item,i) => {
-        if (item.isIntersecting) {item.target.classList.add('block-2-circle-inView');  item.target.style.animationDelay = 0.5 * i + "s"}
-    })
+  benefitBlockImage.forEach((item) => {
+    if (item.isIntersecting) item.target.classList.add("image-wrapper-left-inView");
+  });
 
-    benefitBlockImage.forEach(item => {
-        if (item.isIntersecting) item.target.classList.add('image-wrapper-left-inView')
-    })
+  quoteBlock.forEach((item) => {
+    if (item.isIntersecting) item.target.classList.add("quoteBlock-inView");
+  });
 
-    quoteBlock.forEach(item => {
-        if (item.isIntersecting) item.target.classList.add('quoteBlock-inView')
-    })
-
-    quoteSymbol.forEach(item => {
-        if (item.isIntersecting) item.target.classList.add('quoting-symbol-inView')
-    })
-
+  quoteSymbol.forEach((item) => {
+    if (item.isIntersecting) item.target.classList.add("quoting-symbol-inView");
+  });
 });
 
 //attaches observer to elements which are to be animated
 
 elementsToAnimate.forEach((item) => {
-    observer.observe(item);
+  observer.observe(item);
 });
